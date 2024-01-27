83d6d8667d9caac5c8c10bf6ebcd0c763ecfe912
Arthur Klis <daniel.klis325@o2.pl>
2024-01-27 14:58:41 +0100

    Version 1. Everything is working.

---
+++ source.py
@@ -0,0 +1,32 @@
+# version 1
+# 2023-12-14
+i=1
+listaJednosci=[]
+listaCzesciDziesietnych=[]
+while i:
+ liczba=input(': ')
+ if liczba != "":  
+  listaJednosci.append(int(liczba.split(",")[0]))
+  listaCzesciDziesietnych.append(int(liczba.split(",")[1]))
+ else:
+  break
+nominalyJednosci = {'100': 0, '50': 0, '20': 0, '10': 0, '5': 0, '2': 0, '1': 0}
+nominalyCzesciDziesietnej={'50': 0, '20': 0, '10': 0, '5': 0, '2': 0, '1': 0}
+for liczba in listaJednosci:
+ for key in nominalyJednosci.keys():
+  if liczba >= int(key):
+   nominalyJednosci[key] += int(liczba//int(key))
+   liczba %= int(key)
+for liczba in listaCzesciDziesietnych:
+ for key in nominalyCzesciDziesietnej.keys():
+  if liczba >= int(key):
+   nominalyCzesciDziesietnej[key] += int(liczba//int(key))
+   liczba %= int(key)
+print('') #dodanie by się ładnie wyświetlało w aplikacji iPhone-a
+for k, v in nominalyJednosci.items(): print(k, v)
+for k, v in nominalyCzesciDziesietnej.items():
+ if len(k)==1:
+  k='0.0'+k
+ else:
+  k='0.'+k
+ print(k,v)
