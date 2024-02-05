7d1b43c06243da1ded6a69566c8137bceab05e92
Arthur Klis <daniel.klis325@o2.pl>
2024-02-05 17:47:52 +0100

    Added the total sum of all values

--- source.py
+++ source.py
@@ -1,13 +1,15 @@
-# version 1
-# 2023-12-14
+# version 1.x
+# 2024-02-05
 i=1
 listaJednosci=[]
 listaCzesciDziesietnych=[]
+listaWartosci=[]
 while i:
  liczba=input(': ')
  if liczba != "":  
   listaJednosci.append(int(liczba.split(",")[0]))
   listaCzesciDziesietnych.append(int(liczba.split(",")[1]))
+  listaWartosci.append(float(liczba.replace(',','.')))
  else:
   break
 nominalyJednosci = {'100': 0, '50': 0, '20': 0, '10': 0, '5': 0, '2': 0, '1': 0}
@@ -30,3 +32,4 @@ for k, v in nominalyCzesciDziesietnej.items():
  else:
   k='0.'+k
  print(k,v)
+print(sum(listaWartosci))
